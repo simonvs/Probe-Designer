899bce9517896e58c7d6346fcfc0037dea5d540d
Simón Vergara <56368680+simonvs@users.noreply.github.com>
2023-05-18 23:40:58 -0400

    arreglando problema con los paths

--- descarga.py
+++ descarga.py
@@ -1,13 +1,13 @@
 import os
 import argparse
+import shutil
 from Bio import Entrez, SeqIO
-Entrez.mail = "simonvergaraswett@hotmail.com"
 
 def accnum_to_seqrecord(accesion_number):
     # Función que recibe un accession number de la base de datos 'nucleotide' de NCBI
     # https://www.ncbi.nlm.nih.gov/nuccore.
     # Genera el archivo .gbk (GenBank) en la carpeta 'files' y retorna la secuencia en formato SeqRecord.
-    path = "files/"+accesion_number+".gbk"
+    path = os.path.join("files", accesion_number+".gbk")
     if not os.path.isfile(path):
         print("Descargando secuencia...")
         with Entrez.efetch(db="nucleotide", id=accesion_number, rettype="gb", retmode="text") as handle:
@@ -16,29 +16,34 @@ def accnum_to_seqrecord(accesion_number):
     record = SeqIO.read(path, "genbank")
     return record
 
-def gbk_to_seqrecord(path_to_gbk):
-    # Función que recibe una ruta a un archivo .gbk o .gb (GenBank) y lo retorna como SeqRecord.
-    record = SeqIO.read(path_to_gbk, "genbank")
+def parse_file_to_seqrecord(filepath, type):
+    # Función que recibe el handler de un archivo, su nombre, su tipo y lo retorna como SeqRecord.
+    # Genera una copia del achivo en la carpeta files
+    # 'type' puede ser "genbank", "fasta", "swiss" y otros soportados por SeqIO.
+    currentpath = os.path.abspath(filepath)
+    newpath = os.path.join(os.getcwd(), "files", os.path.split(currentpath)[1])
+    if not os.path.isfile(newpath):
+        shutil.copy(currentpath, newpath)
+
+    record = SeqIO.read(newpath, type)
     return record
 
-def fasta_to_seqrecord(path_to_fasta):
-    # Función que recibe una ruta a un archivo FASTA y lo retorna como SeqRecord.
-    record = SeqIO.read(path_to_fasta, "fasta")
-    return record
-
-def main():    
+def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-an',
                         '--accessionnumber',
                         help='Accesion number de la secuencia a descargar.',
                         required=True,
                         default=argparse.SUPPRESS)
+    parser.add_argument('-db', '--database',help='Base de datos de la que se quiere descargar (nucleotide para secuencias).', type=str, default='nucleotide')
 
     args = parser.parse_args()
     dargs = vars(args)
+    Entrez.mail = "simonvergaraswett@hotmail.com"
 
-    record = accnum_to_seqrecord(accesion_number=dargs["accessionnumber"])
-    print(record)
+    if (dargs["database"] == 'nucleotide'):
+        record = accnum_to_seqrecord(accesion_number=dargs["accessionnumber"])
+        print(len(record))
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -2,6 +2,16 @@ import descarga
 #NG_008617.1
 #AC005600.1
 #NM_000186.4
-record = descarga.accnum_to_seqrecord("NM_000186.4")
+#NC_000016.10
+record = descarga.parse_file_to_seqrecord("C:/Users/simon/Documents/NG_008617.1.gbk", 'genbank')
+exones = 0
 for f in record.features:
-    print(f.type, len(f.location))
+    if f.type == 'exon':
+        exones += 1
+print('Tiene {} exones.'.format(exones))
+record2 = descarga.accnum_to_seqrecord('NC_000016.10')
+exones2 = 0
+for f in record2.features:
+    if f.type == 'exon':
+        exones2 += 1
+print('Tiene {} exones'.format(exones2))
