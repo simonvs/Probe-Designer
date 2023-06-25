77726d5b00d8dabf519f9cdab8d0ab0cf4f3ecb1
Simón Vergara <56368680+simonvs@users.noreply.github.com>
2023-06-25 17:49:23 -0400

    Generador de reporte excel

--- diseno.py
+++ diseno.py
@@ -1,21 +1,58 @@
 import descarga
-
 import argparse
 import pandas as pd
+import primer3
 from Bio.SeqUtils import GC, MeltingTemp
 from Bio.Seq import Seq
+from Bio.Align import PairwiseAligner
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
 
 
-def check_probe(seq, minlen, maxlen, tmmin, tmmax, gcmin, gcmax):
+def check_probe(seq, query, minlen, maxlen, tmmin, tmmax, gcmin, gcmax):
+    """
+    Función que recibe una sonda (secuencia) y las restricciones para verificar que la
+    sonda cumpla los parámetros establecidos.
+    Retorna un valor booleano que determina si cumple las restricciones o no.
+    :param seq: La secuencia que se quiere verificar.
+    :param record:
+    :param minlen: El largo mínimo de la sonda.
+    :param maxlen: El largo máximo de la sonda.
+    :return: La secuencia en formato SeqRecord con todas sus anotaciones.
+    """
+
+    #Chequear largo
     if len(seq) < minlen or len(seq) > maxlen:
         #print('Largo no valido')
         return False
+    
+    #Chequear Tm
     if MeltingTemp.Tm_NN(seq) < tmmin or MeltingTemp.Tm_NN(seq) > tmmax:
         #print('Tm fuera de rango')
         return False
+    
+    #Chequear %GC
     if GC(seq) < gcmin or GC(seq) > gcmax:
         #print('GC fuera de rango')
         return False
+    
+    #Chequear homopolímeros
+    #dg_hairpin = primer3.calcHairpin(str(seq)).dg
+    #dg_homodimero = primer3.calcHomodimer(str(seq)).dg
+
+    #Chequear especificidad usando alineamiento
+    #aligner = PairwiseAligner()
+    #aligner.mode = 'local'
+    #aligner.match_score = 1  # Puntaje por coincidencia
+    #aligner.mismatch_score = -1  # Puntaje por desajuste
+    #aligner.open_gap_score = -1  # Puntaje por abrir un gap
+    #aligner.extend_gap_score = -0.5  # Puntaje por extender un gap
+
+    #alineamiento = aligner.align(seq.complement(), str(query))
+
+    #PENDIENTE: ¿Qué criterio uso para considerarlo específico?
+    #if(sorted(alineamiento)[0].score >= float(len(seq)/4)):
+    #    return False
     return True
 
 
@@ -25,11 +62,14 @@ def get_probe_from_pos(empalme, record, site, minlen, maxlen, tmmin, tmmax, gcmi
     while i <= maxlen and not b:
         if site == -1:
             probe = record.seq[empalme[0]-i:empalme[0]].reverse_complement()
+            query = record.seq[:empalme[0]-i-1] + record.seq[empalme[0]+1:]
         elif site == 1:
             probe = record.seq[empalme[1]:empalme[1]+i].reverse_complement()
+            query = record.seq[:empalme[1]-1] + record.seq[empalme[1]+i+1:]
         else:
-            probe = record.seq[empalme[0]-i//2:empalme[0]] + record.seq[empalme[1]:empalme[1]+i//2].reverse_complement()
-        b = check_probe(probe, minlen, maxlen, tmmin, tmmax, gcmin, gcmax)
+            probe = (record.seq[empalme[0]-i//2:empalme[0]] + record.seq[empalme[1]:empalme[1]+i//2]).reverse_complement()
+            query = record.seq
+        b = check_probe(probe, query, minlen, maxlen, tmmin, tmmax, gcmin, gcmax)
         i += 1
     if not b:
         return Seq('AAA')
@@ -47,7 +87,7 @@ def get_splicing_pairs(locations):
     return empalmes
 
 
-def probe_designer(record, minlen=10, maxlen=50, tmmin=50, tmmax=80, gcmin=30, gcmax=70):
+def probe_designer(record, minlen=60, maxlen=120, tmmin=65, tmmax=75, gcmin=30, gcmax=70):
 
     locations = []
     dict_cds = {
@@ -83,36 +123,100 @@ def probe_designer(record, minlen=10, maxlen=50, tmmin=50, tmmax=80, gcmin=30, g
             donor = get_probe_from_pos(empalme, record, -1, minlen, maxlen, tmmin, tmmax, gcmin, gcmax)
             dict_probes['gen'].append(row['gen'])
             dict_probes['transcrito'].append(row['info'])
-            dict_probes['empalme'].append(empalme)
+            dict_probes['empalme'].append(str(empalme))
             dict_probes['sonda'].append(str(donor))
             dict_probes['lado'].append('donor site')
-            dict_probes['tm'].append(MeltingTemp.Tm_NN(donor))
-            dict_probes['gc'].append(GC(donor))
+            dict_probes['tm'].append(int(MeltingTemp.Tm_NN(donor)))
+            dict_probes['gc'].append(int(GC(donor)))
             dict_probes['largo'].append(len(donor))
 
             acceptor = get_probe_from_pos(empalme, record, 1, minlen, maxlen, tmmin, tmmax, gcmin, gcmax)
             dict_probes['gen'].append(row['gen'])
             dict_probes['transcrito'].append(row['info'])
-            dict_probes['empalme'].append(empalme)
+            dict_probes['empalme'].append(str(empalme))
             dict_probes['sonda'].append(str(acceptor))
             dict_probes['lado'].append('acceptor site')
-            dict_probes['tm'].append(MeltingTemp.Tm_NN(acceptor))
-            dict_probes['gc'].append(GC(acceptor))
+            dict_probes['tm'].append(int(MeltingTemp.Tm_NN(acceptor)))
+            dict_probes['gc'].append(int(GC(acceptor)))
             dict_probes['largo'].append(len(acceptor))
 
             central = get_probe_from_pos(empalme, record, 0, minlen, maxlen, tmmin, tmmax, gcmin, gcmax)
             dict_probes['gen'].append(row['gen'])
             dict_probes['transcrito'].append(row['info'])
-            dict_probes['empalme'].append(empalme)
+            dict_probes['empalme'].append(str(empalme))
             dict_probes['sonda'].append(str(central))
             dict_probes['lado'].append('central')
-            dict_probes['tm'].append(MeltingTemp.Tm_NN(central))
-            dict_probes['gc'].append(GC(central))
+            dict_probes['tm'].append(int(MeltingTemp.Tm_NN(central)))
+            dict_probes['gc'].append(int(GC(central)))
             dict_probes['largo'].append(len(central))
 
     df_probes = pd.DataFrame(dict_probes)
     return df_probes
 
+def generate_xlsx(df, minlen, maxlen, tmmin, tmmax, gcmin, gcmax):
+    wb = Workbook()
+
+    sheet1 = wb.active
+    sheet1.title = 'Parámetros'
+
+    sheet1.cell(row=1, column=1).value = 'PARÁMETROS DE DISEÑO'
+
+    sheet1.cell(row=3, column=1).value = 'Largo de sonda mínimo'
+    sheet1.cell(row=3, column=2).value = minlen
+
+    sheet1.cell(row=4, column=1).value = 'Largo de sonda máximo'
+    sheet1.cell(row=4, column=2).value = maxlen
+
+    sheet1.cell(row=6, column=1).value = 'Temp Melting mínima'
+    sheet1.cell(row=6, column=2).value = tmmin
+
+    sheet1.cell(row=7, column=1).value = 'Temp Melting máxima'
+    sheet1.cell(row=7, column=2).value = tmmax
+
+    sheet1.cell(row=9, column=1).value = '%GC mínimo'
+    sheet1.cell(row=9, column=2).value = gcmin
+
+    sheet1.cell(row=10, column=1).value = '%GC máximo'
+    sheet1.cell(row=10, column=2).value = gcmax
+
+    for column_cells in sheet1.columns:
+        max_length = 2
+        column = column_cells[0].column_letter
+        for cell in column_cells:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2) * 1.2
+        sheet1.column_dimensions[column].width = adjusted_width
+
+
+
+    sheet2 = wb.create_sheet("Sondas")
+    for r in dataframe_to_rows(df, index=True, header=True):
+        sheet2.append(r)
+
+    for cell in sheet2['A'] + sheet2[1]:
+        cell.style = 'Pandas'
+
+    #Ajustar ancho de columnas
+    for column_cells in sheet2.columns:
+        max_length = 2
+        column = column_cells[0].column_letter
+        for cell in column_cells:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2) * 1.2
+        sheet2.column_dimensions[column].width = adjusted_width
+
+    
+
+    wb.save('archivo_excel.xlsx') # nombre: datetime?
+    return 0
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -121,24 +225,32 @@ def main():
                         help='Accesion number de la secuencia a analizar.',
                         required=True,
                         default=argparse.SUPPRESS)
-    parser.add_argument('-minlen', '--minimumlength',help='Largo mínimo de sonda.' ,type=int,default=10)
-    parser.add_argument('-maxlen', '--maximumlength',help='Largo mínimo de sonda.' ,type=int,default=50)
-    parser.add_argument('-tmmin', '--mintempmelting',help='Temperatura de melting mínima.' ,type=int,default=40)
-    parser.add_argument('-tmmax', '--maxtempmelting',help='Temperatura de melting máxima.' ,type=int,default=70)
-    parser.add_argument('-gcmin', '--minumumgc',help='Porcentaje de GC mínimo.' ,type=int,default=35)
-    parser.add_argument('-gcmax', '--maximumgc',help='Porcentaje de GC máximo.' ,type=int,default=65)
+    parser.add_argument('-minlen', '--minimumlength',help='Largo mínimo de sonda.' ,type=int,default=60)
+    parser.add_argument('-maxlen', '--maximumlength',help='Largo mínimo de sonda.' ,type=int,default=120)
+    parser.add_argument('-tmmin', '--mintempmelting',help='Temperatura de melting mínima.' ,type=int,default=65)
+    parser.add_argument('-tmmax', '--maxtempmelting',help='Temperatura de melting máxima.' ,type=int,default=75)
+    parser.add_argument('-gcmin', '--minumumgc',help='Porcentaje de GC mínimo.' ,type=int,default=30)
+    parser.add_argument('-gcmax', '--maximumgc',help='Porcentaje de GC máximo.' ,type=int,default=70)
 
     args = parser.parse_args()
     dargs = vars(args)
-    #rec = descarga.parse_file_to_seqrecord('C:/Users/simon/Downloads/tp53.gb')
-    rec = descarga.accnum_to_seqrecord(dargs["accessionnumber"])
-    probe_designer(record=rec,
+    rec = descarga.parse_file_to_seqrecord('C:/Users/simon/Downloads/tp53.gb')
+    #rec = descarga.accnum_to_seqrecord(dargs["accessionnumber"])
+    df = probe_designer(record=rec,
                    minlen=dargs["minimumlength"],
                    maxlen=dargs["maximumlength"],
                    tmmin=dargs["mintempmelting"],
                    tmmax=dargs["maxtempmelting"],
                    gcmin=dargs["minumumgc"],
-                   gcmax=dargs["maximumgc"]).to_csv('files/probes.csv')
+                   gcmax=dargs["maximumgc"])
+    
+    generate_xlsx(df=df,
+                minlen=dargs["minimumlength"],
+                maxlen=dargs["maximumlength"],
+                tmmin=dargs["mintempmelting"],
+                tmmax=dargs["maxtempmelting"],
+                gcmin=dargs["minumumgc"],
+                gcmax=dargs["maximumgc"])
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,6 +1,35 @@
-from dna_features_viewer import BiopythonTranslator
+# from dna_features_viewer import BiopythonTranslator
 
-graphic_record = BiopythonTranslator().translate_record("C:/Users/simon/Downloads/tp53.gb")
-ax, _ = graphic_record.plot(figure_width=10, strand_in_label_threshold=7)
-ax.figure.tight_layout()
-ax.figure.savefig('sequence_and_translation.png')
+# graphic_record = BiopythonTranslator().translate_record("C:/Users/simon/Downloads/tp53.gb")
+# ax, _ = graphic_record.plot(figure_width=10, strand_in_label_threshold=7)
+# ax.figure.tight_layout()
+# ax.figure.savefig('sequence_and_translation.png')
+#
+#####################################################
+#
+# from Bio.Align import PairwiseAligner
+
+# # Crear un objeto PairwiseAligner
+# aligner = PairwiseAligner()
+
+# # Definir las dos secuencias que deseas alinear
+# secuencia1 = "ACGT"
+# secuencia2 = "GGGGGGACGGTGAAAATGTAACGGGT"
+
+# # Configurar los parámetros del alineador
+# aligner.mode = 'local'  # Alineamiento global
+# aligner.match_score = 1  # Puntaje por coincidencia
+# aligner.mismatch_score = -1  # Puntaje por desajuste
+# aligner.open_gap_score = -1  # Puntaje por abrir un gap
+# aligner.extend_gap_score = -0.5  # Puntaje por extender un gap
+
+# # Realizar el alineamiento entre las dos secuencias
+# alineamiento = aligner.align(secuencia1, secuencia2)
+
+# print(sorted(alineamiento)[0])
+# #for alignment in sorted(alineamiento):
+# #    print("Score = %.3f:" % alignment.score)
+# #    print(alignment)
+
+import re
+print([match.group() for match in re.finditer(r"([A-Z]+)\1{1,}", "ACTGTAGGGCTGTACTACTACTATG")])
