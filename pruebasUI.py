f5856fe42b34c6ed170a72398758a57d1946a41b
Simón Vergara <56368680+simonvs@users.noreply.github.com>
2023-10-25 00:48:46 -0300

    limpiando

--- pruebasUI.py
+++
@@ -1,9 +0,0 @@
-seq = 'AGAGAGTGACGTA'
-print(seq[0:2]*3)
-checked = []
-for i in range(len(seq)-2):
-    subseq = str(seq[i:i+2])
-    if subseq not in checked:
-        if subseq * 3 in seq:
-            print('Homopolimero de 2 nucleotidos')
-        checked.append(subseq)
